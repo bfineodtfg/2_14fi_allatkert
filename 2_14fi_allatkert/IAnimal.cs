68215563113248e15e479579bd584dc8054f8236
zsar.daniel <zsar.daniel@C19-TANAR>
2025-01-16 20:03:40 +0100

    Add project files.

---
+++ 2_14fi_allatkert/IAnimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_14fi_allatkert
+{
+    interface IAnimal
+    {
+        string animalName { get; set; }
+        int age { get; set; }
+        int weight { get; set; }
+        string typeOFanimal { get; set; }
+        string foodType { get; set; }
+        Label nameLabel { get; set; }
+        Label typeLabel { get; set; }
+    }
+}
---
+++ 2_14fi_allatkert/Lion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_14fi_allatkert
+{
+    class Lion: UserControl, IAnimal
+    {
+        public Lion(string name, string type) {
+            animalName = name;
+            typeOFanimal = type;
+            nameLabel = new Label() { Text = name };
+            typeLabel = new Label() { Text = type };
+        }
+        public Label nameLabel { get; set; }
+        public Label typeLabel { get; set; }
+        public string animalName { get; set; }
+        public int age { get; set; }
+        public int weight { get; set; }
+        public string typeOFanimal { get; set; }
+        public string foodType { get; set; }
+    }
+}
